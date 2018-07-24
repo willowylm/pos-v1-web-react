00f76579fc0ff8b4b6e37503e5e3caa3e56c3a7d
Liuming Yang <ylmap@126.com>
2018-07-25 00:06:44 +0800

    display data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Product from './displayProduct.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       app
+       <div>
+          <Product />
+        </div>
       </div>
     );
   }
---
+++ src/displayProduct.js
@@ -0,0 +1,51 @@
+import React, { PureComponent } from "react";
+import "./App.css";
+import { loadAllItems, loadPromotions } from "./datbase";
+const items = loadAllItems();
+const chargeitems = loadPromotions(); 
+
+class Product extends PureComponent {
+  constructor(pros) {
+    super(pros), 
+    (this.state = {});
+  }
+
+  chargeImformation = (chargeitems) =>{
+      items.forEach(item => {
+          if(item.concat(chargeitems)) 
+             return chargeitems.type; 
+      });
+  };
+
+  render() {
+    return(
+      <div>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>名称</th>
+              <th>单位</th>
+              <th>单价</th>
+              <th>优惠信息</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map(item => {
+              return (
+                <tr key={item.barcode}>
+                  <td>{item.name}</td>
+                  <td>{item.unit}</td>
+                  <td>{item.price}</td>
+                  <td onChange="chargeImformation()"></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+
+export default Product;
